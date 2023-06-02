de382cccd677eb23db1edfd1baa9ded23ae4281d
Fraser Darwent <fraser@darwen.tf>
2023-06-02 12:11:42 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Hello, My Svelte World!!</title>
+</svelte:head>
+
+<slot />
+
+<style lang="scss">
+	@use '../theme';
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
+</style>
